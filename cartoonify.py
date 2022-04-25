f473d7c67c4a59e3dd68e84e705ce5c3be296766
atakhadivi <atakhadivi@gmail.com>
2022-04-25 10:26:35 +0430

    first commit

---
+++ cartoonify.py
@@ -0,0 +1,12 @@
+import cv2
+import easygui
+import numpy as np
+import imageio 
+import sys 
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image,ImageTk
+
